aeba7564e53fec2025fcd5d140d24be1ad853db9
HuiXia Gao <galina@fooyo.sg>
2018-06-20 14:26:32 +0800

    demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <span>hello</span>
+    <div class="breathe"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@keyframes breathe {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+    transform: scale3d(1.3, 1.3, 1.3);
+  }
+}
+.hello {
+  background-color: #0fdcff;
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #ffffff;
+  margin: 0 auto;
+  position: relative;
+}
+
+.breathe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background-color: rgb(15, 220, 255, 0.4);
+  animation: breathe 2s ease-in-out infinite;
+}
+</style>
